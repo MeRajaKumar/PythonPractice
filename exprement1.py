1febd8e0d21d18db094fae294df62f6522cab489
Raja Kumar <teamaayushgupta@gmail.com>
2024-08-13 08:26:43 +0530

    some commits done today

---
+++ exprement1.py
@@ -0,0 +1,23 @@
+list1=[12,45,4,8,13,56,87]
+print(list1[0])
+print(list1[2:4])
+list1.insert(3,56)
+print(list1)
+list1.append(27)
+print(list1)
+list1.remove(13)
+print(list1)
+list2 = [2,89,4,46,19,76]
+list2.sort()
+print(list2)
+list2 = [2,13,4,46,13,76]
+list2.reverse()
+print(list2)
+list2 = [2,13,4,46,13,76]
+print(len(list2))
+list2 = [2,13,4,46,13,76]
+for x in list2:
+     print(x) 
+
+
+
--- fisrt.py
+++
@@ -1,5 +0,0 @@
-num1 = 5
-num2 = 10
-tot = num1 + num2
-print(tot)
-print("Hello World")
