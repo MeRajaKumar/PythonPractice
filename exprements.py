f6454fd07712009d1b0e83b3aa1add26d1154478
Raja Kumar <teamaayushgupta@gmail.com>
2024-08-23 07:19:13 +0530

    changes of the day

--- exprements.py
+++ exprements.py
@@ -1,24 +1,24 @@
 # Aim: WAP to perform list operation-creating, accessing, slicing, modifying, adding, removing, sorting, reversing, finding length and looping through lists. 
-list1=[12,45,4,8,13,56,87] #creating
-print(list1[0]) #accessing
-print(list1[2:4]) #slicing
-list1.insert(3,56) #modifying
-print(list1)
-list1.append(27) #adding
-print(list1)
-list1.remove(13) #removing
-print(list1)
-list2 = [2,89,4,46,19,76]
-list2.sort() #sorting
-print(list2)
-list2 = [2,13,4,46,13,76]
-list2.reverse() #reversing
-print(list2)
-list2 = [2,13,4,46,13,76]
-print(len(list2)) #finding length
-list2 = [2,13,4,46,13,76]
-for x in list2:
-     print(x) 
+# list1=[12,45,4,8,13,56,87] #creating
+# print(list1[0]) #accessing
+# print(list1[2:4]) #slicing
+# list1.insert(3,56) #modifying
+# print(list1)
+# list1.append(27) #adding
+# print(list1)
+# list1.remove(13) #removing
+# print(list1)
+# list2 = [2,89,4,46,19,76]
+# list2.sort() #sorting
+# print(list2)
+# list2 = [2,13,4,46,13,76]
+# list2.reverse() #reversing
+# print(list2)
+# list2 = [2,13,4,46,13,76]
+# print(len(list2)) #finding length
+# list2 = [2,13,4,46,13,76]
+# for x in list2:
+#      print(x) 
 
 
 # 2️⃣ Write a program to print twin prime less than thousand. (How to calculate twin prime in range of thousand). If two consecutive odd number are both prime then they are knows as twin primes. 
@@ -30,6 +30,20 @@ for x in list2:
 #                break
 #        else:
 #            print(f"{num} is a prime number!")
+
+# 2️⃣ method
+# def prime(n):
+#      f=0
+#      for i in range(2,(n//2)+1):
+#           if n%i==0:
+#                f=1
+#                break
+#      return f
+# last_count=int(input("Enter last number : "))
+# print("Twin prime numbers are: ")
+# for i in range(2,last_count+1):
+#      if prime(i)==0 and prime(i+2)==0:
+#           print(i," ",i+2)
 
 
 # for i in range (2,1000):
