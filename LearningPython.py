7c36fb12fe0fd690f5e7e1b06737ae7cbc131778
Raja Kumar <teamaayushgupta@gmail.com>
2024-09-03 14:36:58 +0530

    changes if te day

---
+++ LearningPython.py
@@ -0,0 +1,39 @@
+#lecture no - 18
+# a=5
+# b='5'
+# b=5
+# print(id(a))
+# print(id(b))
+# print(a is b)
+# print(a is not b)
+
+#lecture no - 12
+# firstNumber=input("Enter 1st number: ")
+# secondNumber=input("Enter 1st number: ")
+# sum=firstNumber+secondNumber
+# print(sum) #it will add 1 to 2
+# firstNumber=int(input("Enter 1st number: "))
+# secondNumber=int(input("Enter 1st number: "))
+# sum=firstNumber+secondNumber
+# print(sum)
+
+#lecture no - 11
+# var_1=123
+# var_2=10.1
+# print(var_1+var_2) 
+# print(type(var_1))
+# print(type(var_2))
+# a=1
+# b=2
+# var=a<b
+# print(var)
+# print(type(var))
+
+#lecture no - 09
+# a=input("enter value of a: ")
+# b=input("enter value for b: ")
+# temp=a
+# a=b
+# b=temp
+# print("now values of a: ",a)
+# print("now values of a: ",b)
