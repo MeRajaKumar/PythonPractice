cec359085c5bd24227f6a1aa6a5f28dd697da66e
Raja Kumar <teamaayushgupta@gmail.com>
2024-09-12 19:54:50 +0530

    all changes commited

---
+++ 11sep.py
@@ -0,0 +1,8 @@
+import numpy as np
+# arr=np.array([[[1,2,3],[4,5,6],[7,8,9]]])
+arr=np.array([[[1,2,3,4,5]]]) 3 2d array
+arr=np.array([])
+for x in arr:
+     for y in x:
+          for j in y:
+               print(j)
