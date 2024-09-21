e4743106f15e45684aba98d2bb84dcd01c417dbc
Raja Kumar <teamaayushgupta@gmail.com>
2024-09-21 13:03:14 +0530

    all changes for the day

---
+++ 20sep.py
@@ -0,0 +1,27 @@
+# import numpy as np
+# arr=np.array([[[ [[1,2,3,4],[5,6,7,8]] ]]])
+# print(arr)
+# print(arr.ndim)
+
+# print("Simple Calculator!")
+# operation = int(input("Enter your operation: (1:Addition/n2:Subtractions/n3:Multiplication/n4:division/n5:Module"))
+# num1=int(input("Enter your 1st value: "))
+# num2=int(input("Enter your 2nd values: "))
+# cal={}
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints,'*--r')
+plt.show()
+
+# xpoints = np.array([0, 6])
+# ypoints = np.array([0, 250])
+
+# plt.plot(xpoints, ypoints)
+# plt.show()
+
+
+
