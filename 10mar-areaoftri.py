802a3defda78ba7b7ac86fcad75f3a1db27f34cc
Raja Kumar <teamaayushgupta@gmail.com>
2025-03-10 18:04:36 +0530

    initial commit

---
+++ 10mar-areaoftri.py
@@ -0,0 +1,8 @@
+# Write a Python program to calculate the area of a rectangle given its length and width?
+
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+area = length * width
+
+print("The area of the rectangle is:", area)
