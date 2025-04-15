a2a31c6f05afb679409de110cd5a014b8cf5316d
Raja Kumar <teamaayushgupta@gmail.com>
2025-04-15 15:54:31 +0530

    initail commit

---
+++ Experiments/exp6-daa.py
@@ -0,0 +1,30 @@
+def find_subsets(S, target):
+    result = []
+
+    def backtrack(start, path, current_sum, mask):
+        if current_sum == target:
+            result.append((path[:], mask[:]))
+            return
+        if current_sum > target:
+            return
+
+        for i in range(start, len(S)):
+            path.append(S[i])
+            mask[i] = 1
+            backtrack(i + 1, path, current_sum + S[i], mask)
+            path.pop()
+            mask[i] = 0
+
+    backtrack(0, [], 0, [0]*len(S))
+
+    if result:
+        print("Subsets with sum", target, "are:")
+        for subset, binary_mask in result:
+            print("Subset:", subset, "→", binary_mask)
+    else:
+        print("No subset found with the given sum.")
+
+# Example usage
+S = [1, 3, 7, 11]
+d = 10
+find_subsets(S, d)
